635e903e57da9f7a5eb902cb3ba522afba13da4c
LindaBot <voidnullbot@outlook.com>
2018-11-12 21:18:32 +1300

    Added mongoDB connection using mongoose

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
+mongoose.connect('mongodb+srv://admin:'+ process.env.MONGO_ATLAST_PW +'@node-rest-shopapi-7qkjo.azure.mongodb.net/test?retryWrites=true',
+{
+    useMongoClient: true
+});
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
 
 // Add header
 app.use((req, res, next) => {
